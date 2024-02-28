9651f580283e734335f85330f7ac36d52b812ab4
Joe Bush <joebush4444@gmail.com>
2024-02-28 13:22:35 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Joe Bush",
+    streetAddress: "1234 Sesame St."
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+   let updatedEmployee = { ...employee };
+    updatedEmployee[key]=value;
+    return updatedEmployee;
+};
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key]=value;
+    return employee;
+};
+function deleteFromEmployeeByKey(employee,key){
+    let updatedEmployee = {...employee};
+    delete updatedEmployee[key];
+    return updatedEmployee;
+};
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key];
+    return employee;
+};
